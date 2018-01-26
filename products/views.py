84259acb25e228260bb2468ca8aea649e1d4a7d9
blog <rubygig@yahoo.com>
2018-01-26 17:37:38 +0530

    second

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ class ProductListView(ListView):
 def product_list_view(request):
 	queryset = Product.objects.all()
 	context = {
-		'object': queryset
+		'object_list': queryset
 	}
 	return render(request, "products/list.html", context)
 class ProductDetailView(DetailView):
